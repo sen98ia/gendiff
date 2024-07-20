c5d5d503c62da57b9655b2069252eee7e94cd9cd
sen98ia <sen98ia@gmail.com>
2024-07-20 22:14:05 +0300

    add ./src/

---
+++ src/genDiff.js
@@ -0,0 +1,9 @@
+import { program } from 'commander';
+
+export default program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information');
+
+program.parse();
