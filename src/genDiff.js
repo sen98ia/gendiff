862bbcf018f36b58fb108bfbde8eae09b4b2fa4c
sen98ia <sen98ia@gmail.com>
2024-07-21 15:39:42 +0300

    update src/genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,6 +4,8 @@ export default program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
