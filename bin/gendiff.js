bf4edcbb4846184813b01474e1ce981c87e3a96b
sen98ia <sen98ia@gmail.com>
2024-07-21 19:21:26 +0300

    modify bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,11 @@
-import genDiff from '../src/genDiff.js';
+import { program } from 'commander';
 
-genDiff();
+export default program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
+
+program.parse();
