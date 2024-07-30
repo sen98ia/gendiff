ad7c2f50da692b1b7d4aafd27d07177c990a5c7c
sen98ia <sen98ia@gmail.com>
2024-07-30 23:03:10 +0300

    update src/parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,3 +1,20 @@
-const parse = (data) => JSON.parse(data);
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+
+const readFile = (filePath) => fs.readFileSync(filePath);
+const getFormat = (filePath) => path.extname(filePath);
+
+const parse = (filePath) => {
+  const format = getFormat(filePath);
+  const data = readFile(filePath);
+  if (format === '.json') {
+    return JSON.parse(data);
+  }
+  if (format === '.yaml' || format === '.yml') {
+    return yaml.load(data);
+  }
+  throw new Error(`Invalid format - ${format}`);
+};
 
 export default parse;
