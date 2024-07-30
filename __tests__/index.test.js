9cf540c9d422c99059de98d6ddac9ae24be26120
sen98ia <sen98ia@gmail.com>
2024-07-30 23:01:26 +0300

    update __tests__/index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,17 @@ import genDiff from '../src/index.js';
 
 // проверяю тесты в лоб
 
-test('plain files test', () => {
+test('compare flat .json', () => {
   const expectedResult = fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8');
   expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(expectedResult);
+});
+
+test('compare flat .yaml', () => {
+  const expectedResult = fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8');
+  expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toBe(expectedResult);
+});
+
+test('compare flat .yaml and .json', () => {
+  const expectedResult = fs.readFileSync('./__fixtures__/expectedResult.txt', 'utf-8');
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.yaml')).toBe(expectedResult);
 });
