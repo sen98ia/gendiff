5e37613b86db010d929c5d47eaa288ffd5ea649f
sen98ia <sen98ia@gmail.com>
2024-07-30 23:02:53 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import path from 'path';
-import fs from 'fs';
 import parse from './parse.js';
 import flatDiff from './flatDiff.js';
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
-const readFile = (filePath) => fs.readFileSync(filePath);
-const getParsedFile = (filePath) => parse(readFile(filePath));
 
 const genDiff = (filePath1, filePath2) => {
   const absolutePath1 = getAbsolutePath(filePath1);
   const absolutePath2 = getAbsolutePath(filePath2);
 
-  const parsedFile1 = getParsedFile(absolutePath1);
-  const parsedFile2 = getParsedFile(absolutePath2);
+  const parsedFile1 = parse(absolutePath1);
+  const parsedFile2 = parse(absolutePath2);
 
   return flatDiff(parsedFile1, parsedFile2);
 };
