1593a03827a075bdce6cbd208695f662178a0442
sen98ia <sen98ia@gmail.com>
2024-08-04 22:34:39 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import path from 'path';
 import parse from './parse.js';
-import flatDiff from './flatDiff.js';
+import diffTree from './diffTree.js';
+// import stylish from './formatters/stylish.js';
+import plain from './formatters/plain.js';
 
 const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 
-const genDiff = (filePath1, filePath2) => {
+const genDiff = (filePath1, filePath2, formatter = plain) => {
   const absolutePath1 = getAbsolutePath(filePath1);
   const absolutePath2 = getAbsolutePath(filePath2);
 
   const parsedFile1 = parse(absolutePath1);
   const parsedFile2 = parse(absolutePath2);
 
-  return flatDiff(parsedFile1, parsedFile2);
+  const differenceTree = diffTree(parsedFile1, parsedFile2);
+  return formatter(differenceTree);
 };
 
 export default genDiff;
