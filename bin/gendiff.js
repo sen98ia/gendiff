18b8ca735d5a049cba17ca54e79ebc86e975b618
sen98ia <sen98ia@gmail.com>
2024-08-04 22:33:42 +0300

    update bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,10 @@ export default program
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+  .option('-f, --format [type]', 'output format', 'plain')
+  .action((filepath1, filepath2) => {
+    const formatter = program.opts().format;
+    console.log(genDiff(filepath1, filepath2, formatter));
+  });
 
 program.parse(process.argv);
