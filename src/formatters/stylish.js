b337a932758ed28541f48b2081f33ca59e0d3089
sen98ia <sen98ia@gmail.com>
2024-08-07 14:46:44 +0300

    update src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,39 @@
 // import _ from 'lodash';
-import diffTree from '../diffTree.js';
-import parse from '../parse.js';
+// import diffTree from '../diffTree.js';
+// import parse from '../parse.js';
+
+const states = {
+  added: '+ ',
+  removed: '- ',
+  unchanged: '  ',
+  nested: '  ',
+  updated: ['- ', '+ '],
+};
 
 const stylish = (tree) => {
-  const inner = (node) => {
-    if (node.state !== 'nested') {
-      return [node.state, node.key, node.value].join(' ');
+  const innerFunc = (node, depth) => {
+    const spacesCount = 4;
+    const leftIndent = 2;
+    const indentSize = depth * spacesCount - leftIndent;
+    const blank = '.';
+
+    const lines = [];
+
+    if (node.state !== 'nested' && node.state !== 'updated') {
+      lines.push(`${blank.repeat(indentSize)}${states[node.state]}${node.key}: ${node.value}`);
     }
-    return inner(node.value);
+    if (node.state === 'updated') {
+      lines.push(`${blank.repeat(indentSize)}${states[node.state][0]}${node.key}: ${node.oldValue}\n${blank.repeat(indentSize)}${states[node.state][1]}${node.key}: ${node.newValue}`);
+    }
+    if (node.state === 'nested') {
+      lines.push(`${blank.repeat(indentSize)}${states[node.state]}${node.key}: \n${node.value.map((el) => innerFunc(el, depth + 1)).join('\n')}`);
+    }
+    return lines;
   };
-  return tree.map((el) => inner(el));
+  return tree.flatMap((el) => innerFunc(el, 1)).join('\n');
 };
 
 export default stylish;
-
+/*
 console.log(stylish(diffTree(parse('__fixtures__/file1.json'), parse('__fixtures__/file2.json'))));
+*/
