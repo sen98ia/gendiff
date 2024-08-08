5c715ff0de54a8a9057b12befd43abff7aa6346c
sen98ia <sen98ia@gmail.com>
2024-08-08 12:42:51 +0300

    update src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,4 @@
-// import _ from 'lodash';
-// import diffTree from '../diffTree.js';
-// import parse from '../parse.js';
+import _ from 'lodash';
 
 const states = {
   added: '+ ',
@@ -10,30 +8,43 @@ const states = {
   updated: ['- ', '+ '],
 };
 
+const defineValue = (value, depth) => {
+  const spacesCount = 4;
+  const indentSize = depth * spacesCount + spacesCount;
+  const bracketIndent = indentSize - spacesCount;
+  const blank = ' ';
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const entries = Object.entries(value);
+  const result = entries.map(([key, val]) => `${blank.repeat(indentSize)}${key}: ${defineValue(val, depth + 1)}`);
+  return ['{', ...result, `${blank.repeat(bracketIndent)}}`].join('\n');
+};
+
 const stylish = (tree) => {
   const innerFunc = (node, depth) => {
     const spacesCount = 4;
     const leftIndent = 2;
     const indentSize = depth * spacesCount - leftIndent;
-    const blank = '.';
+    const blank = ' ';
+    const bracketIndent = indentSize + leftIndent;
 
-    const lines = [];
+    const {
+      key, state, value, oldValue, newValue,
+    } = node;
 
     if (node.state !== 'nested' && node.state !== 'updated') {
-      lines.push(`${blank.repeat(indentSize)}${states[node.state]}${node.key}: ${node.value}`);
+      return `${blank.repeat(indentSize)}${states[state]}${key}: ${defineValue(value, depth)}`;
     }
     if (node.state === 'updated') {
-      lines.push(`${blank.repeat(indentSize)}${states[node.state][0]}${node.key}: ${node.oldValue}\n${blank.repeat(indentSize)}${states[node.state][1]}${node.key}: ${node.newValue}`);
+      return `${blank.repeat(indentSize)}${states[state][0]}${key}: ${defineValue(oldValue, depth)}\n${blank.repeat(indentSize)}${states[state][1]}${key}: ${defineValue(newValue, depth)}`;
     }
     if (node.state === 'nested') {
-      lines.push(`${blank.repeat(indentSize)}${states[node.state]}${node.key}: \n${node.value.map((el) => innerFunc(el, depth + 1)).join('\n')}`);
+      return `${blank.repeat(indentSize)}${states[state]}${key}: {\n${node.value.map((el) => innerFunc(el, depth + 1)).join('\n')}\n${blank.repeat(bracketIndent)}}`;
     }
-    return lines;
+    throw new Error(`Invalid node state - ${state}`);
   };
-  return tree.flatMap((el) => innerFunc(el, 1)).join('\n');
+  return `{\n${tree.map((el) => innerFunc(el, 1)).join('\n')}\n}`;
 };
 
 export default stylish;
-/*
-console.log(stylish(diffTree(parse('__fixtures__/file1.json'), parse('__fixtures__/file2.json'))));
-*/
