bd234b036fc0d69de3099e14efadb94b993e71e3
sen98ia <sen98ia@gmail.com>
2024-08-08 12:41:49 +0300

    set stylish as default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ export default program
   .version('1.0.0', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'plain')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const formatter = program.opts().format;
     console.log(genDiff(filepath1, filepath2, formatter));
