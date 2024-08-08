ab48be7e11c6e2745cbe46696cf35ed60c63622f
sen98ia <sen98ia@gmail.com>
2024-08-08 12:41:06 +0300

    update with yml case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,16 +10,16 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const extentions = ['json', 'yaml'];
+const extentions = ['json', 'yaml', 'yml'];
 
 test.each(extentions)('test formatters', (extention) => {
   const filePath1 = getFixturePath(`file1.${extention}`);
   const filePath2 = getFixturePath(`file2.${extention}`);
-  // const expectedStylish = readFile('expectedStylish.txt');
+  const expectedStylish = readFile('expectedStylish.txt');
   const expectedPlain = readFile('expectedPlain.txt');
   const expectedJSON = readFile('expectedJson.json');
 
-  // expect(genDiff(filePath1, filePath2)).toBe(expectedStylish);
+  expect(genDiff(filePath1, filePath2)).toBe(expectedStylish);
   expect(genDiff(filePath1, filePath2, 'plain')).toBe(expectedPlain);
   expect(genDiff(filePath1, filePath2, 'json')).toBe(expectedJSON);
 });
